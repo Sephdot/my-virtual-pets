adf51d3536b72b9fc6e60ccb1c64621e713c3547
Joseph Eaton <josepheaton04@outlook.com>
2025-01-30 09:59:38 +0000

    chore: add empty Controller, Repository and Service classes for /users endpoint

---
+++ my-virtual-pets/my-virtual-pets-api/Controllers/UsersController.cs
@@ -0,0 +1,6 @@
+﻿namespace my_virtual_pets_api.Controllers
+{
+    public class UsersController
+    {
+    }
+}
---
+++ my-virtual-pets/my-virtual-pets-api/Services/UsersService.cs
@@ -0,0 +1,6 @@
+﻿namespace my_virtual_pets_api.Services
+{
+    public class UsersService
+    {
+    }
+}
