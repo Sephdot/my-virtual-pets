6fc42c7e798f54de1edc0659ce324bc110609318
Lili <lili.berenyi@gmail.com>
2025-01-31 14:39:37 +0000

    feat: created imageService layer and added removeBackground web client

---
+++ my-virtual-pets/my-virtual-pets-api/Controllers/ImagesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using my_virtual_pets_api.Services;
+
+namespace my_virtual_pets_api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ImagesController : ControllerBase
+{
+    private IImagesService imagesService;
+
+    public ImagesController(IImagesService imagesService)
+    {
+        this.imagesService = imagesService;
+    }
+
+    [HttpGet]
+    [Route("test")]
+    public async Task<IActionResult> GetTestImage(string inputImageUrl)
+    {
+        //string inputImagePath = "Resources/TestImages/DogWithBackground.jpg";
+        //byte[] inputImage = await System.IO.File.ReadAllBytesAsync(inputImagePath);
+        //byte[]? outputImage = await imagesService.RemoveBackground(inputImage);
+        //if (outputImage == null) return BadRequest("Output image was null");
+        //return File(outputImage, "image/png");
+
+        string? outputImageUrl = await imagesService.RemoveBackground(inputImageUrl);
+        if (outputImageUrl == null) return BadRequest("Output image was null");
+        return Ok(outputImageUrl);
+    }
+}
---
+++ my-virtual-pets/my-virtual-pets-api/Services/ApiResponses/BgRemoverResponseData.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.Json.Serialization;
+
+namespace my_virtual_pets_api.Services.ApiResponses;
+public class BgRemoverResponseData
+{
+    [JsonPropertyName("id")]
+    public string Id { get; set; }
+
+    [JsonPropertyName("url")]
+    public string Url { get; set; }
+}
