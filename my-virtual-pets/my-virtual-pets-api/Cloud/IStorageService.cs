568f5ef0cbe9f8ae7b9f33da8906d4e90e6ac734
Joseph Eaton <josepheaton04@outlook.com>
2025-01-31 15:48:46 +0000

    fix: add interface and extend existing storage service from it

---
+++ my-virtual-pets/my-virtual-pets-api/Cloud/IStorageService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace my_virtual_pets_api.Cloud
+{
+    public interface IS3StorageService
+    {
+        Task<(bool, string)> UploadFileAsync(string keyName);
+        Task UploadObject(byte[] imgToUpload, string key);
+    }
+}
