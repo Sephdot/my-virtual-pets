a78c6302ad4cd460d494e296f52cf02e9a34d001
stripi <cgm998cgm@gmail.com>
2025-02-03 09:59:07 +0000

    feat: Implement Image recognition API

---
+++ my-virtual-pets/my-virtual-pets-api/Services/IRecognitionService.cs
@@ -0,0 +1,12 @@
+﻿
+namespace ImageRecognition
+{
+    public interface IRecognitionService
+    {
+        string Model { get; set; }
+
+        Task<string> CheckImageInput(byte[] imageData);
+        Task<string> CheckImageInput(string imageLocation);
+        Task<IPredicted?> Deserialize(string predictionJson);
+    }
+}
