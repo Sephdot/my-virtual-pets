7994acc410701375988bc9979df3ec4930a08227
stripi <cgm998cgm@gmail.com>
2025-02-05 10:23:22 +0000

    Initialise testing branch

---
+++ my-virtual-pets/my-virtual-pets.Tests/DragonEyeAPITests.cs
@@ -0,0 +1,27 @@
+﻿using my_virtual_pets_api.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+
+namespace my_virtual_pets.Tests
+{
+    public class DragonEyeAPITests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Mock<IImagesService> _mockImageService = new Mock<IImagesService>();
+            IImagesService _imageService = _mockImageService.Object;
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
