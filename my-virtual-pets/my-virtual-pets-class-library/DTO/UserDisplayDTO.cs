340a1b980936e2a2efb8005fac7b2fef21fab8e1
kkaisi3 <kkaisi2003@gmail.com>
2025-02-06 11:33:36 +0000

    chore: Add UserDTO file

---
+++ my-virtual-pets/my-virtual-pets-class-library/DTO/UserDisplayDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace my_virtual_pets_class_library.DTO
+{
+    public class UserDisplayDTO
+    {
+        public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int PetCount { get; set; }
+    }
+}
