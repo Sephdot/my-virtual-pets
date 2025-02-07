2692088cf799d395b65f5191f54fbe0dbf7faf7f
stripi <cgm998cgm@gmail.com>
2025-02-07 09:51:44 +0000

    refactor: Move Recognition interfaces to correct folder

---
+++ my-virtual-pets/my-virtual-pets-api/Services/Interfaces/IPredicted.cs
@@ -0,0 +1,17 @@
+﻿using ImageRecognition;
+
+namespace my_virtual_pets_api.Services.Interfaces
+{
+    public interface IPredicted
+    {
+        public long id { get; set; }
+        public string type { get; set; }
+        public string name { get; set; }
+        public string displayName { get; set; }
+        public object score { get; set; }
+        public double uncalibrated_score { get; set; }
+        public List<Child> children { get; set; }
+        public int rarity { get; set; }
+    }
+
+}
---
+++ my-virtual-pets/my-virtual-pets-api/Services/Interfaces/IRecognitionService.cs
@@ -0,0 +1,11 @@
+﻿namespace my_virtual_pets_api.Services.Interfaces
+{
+    public interface IRecognitionService
+    {
+        string Model { get; set; }
+
+        Task<IPredicted> CheckImageInput(byte[] imageData);
+        Task<IPredicted> CheckImageInput(string imageLocation);
+        Task<IPredicted?> Deserialize(string predictionJson);
+    }
+}
