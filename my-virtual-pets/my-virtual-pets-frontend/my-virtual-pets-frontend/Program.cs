875a74fd9557c7f5b62ecf1d5feb2fad3d8cd3b9
Tookey <rachel.tookey@gmail.com>
2025-02-07 22:36:52 +0000

    Feat: username live search

--- my-virtual-pets/my-virtual-pets-frontend/my-virtual-pets-frontend/Program.cs
+++ my-virtual-pets/my-virtual-pets-frontend/my-virtual-pets-frontend/Program.cs
@@ -7,7 +7,6 @@ using my_virtual_pets_frontend.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
@@ -30,7 +29,6 @@ builder.Services.AddAuthorizationCore();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseWebAssemblyDebugging();
@@ -38,7 +36,6 @@ if (app.Environment.IsDevelopment())
 else
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
