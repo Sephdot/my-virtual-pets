28e84f3e639054a069725de0d919b15b67ba478d
Joseph Eaton <josepheaton04@outlook.com>
2025-02-11 15:19:09 +0000

    feat: add static class for making pet types strings

---
+++ my-virtual-pets/my-virtual-pets-class-library/DTO/FriendlyStringifiers/FriendlyStringifiers.cs
@@ -0,0 +1,11 @@
+using my_virtual_pets_class_library.Enums;
+
+namespace my_virtual_pets_class_library.DTO.FriendlyStringifiers;
+
+public static class FriendlyStringifiers
+{
+    public static string FriendlyStringify(this PetType petType)
+    {
+        return petType.ToString().ToLower().Replace('_', ' ');
+    }
+}
